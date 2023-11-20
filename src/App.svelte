9ed78f97ab90282b29c189c9e1784dfddd44b5c7
HatemKhabir <hatemkhabir.cse@gmail.com>
2023-11-20 21:21:29 +0100

    warming up with svelte

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+import Button from "./lib/Button.svelte";
+</script>
+<Button title="khabeyer "/>
+<style>
+
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,23 @@
+<script>
+  //props
+  export let initialCount;
+  export let max;
+let count=initialCount;
+let string=""
+$:{
+  string=`the count is ${count}`
+}
+
+</script>
+<button on:click={()=>{
+    if(count<max) 
+  count++
+}}>
+enzelnii </button>
+<h1>{count}</h1>
+<h2>{string}</h2>
+<style>
+  button{
+    padding: 1%;
+  }
+</style>
